4ef470dd0ea5e764ba273a75318a72688d09bf93
Durolicana <115071928+Durolicana@users.noreply.github.com>
2022-10-19 09:55:01 +0200

    Slettet kode

--- Part1/newton_raphson.py
+++ Part1/newton_raphson.py
@@ -8,8 +8,14 @@ import cmath
 from NR_functions import Ybus, read_buses, P_calc, Q_calc, get_PQ_calc, make_jacobian, delta_VD, updateVD, updateVD_vec, updatePQ_vec 
 from NR_classdef import Network, Buses, PQ, VD
 
+bus_vec = read_buses('Busdata.csv')
 
+power_network = Network(bus_vec)
 
+def NR(Ybus, V, delta, power_network ):
+    
+    
+    num_buses = len(bus_num_init)
 
 
 
