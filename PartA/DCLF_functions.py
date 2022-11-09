2f6351088c4c031fa6082aff75d76719dcc649df
AsbNisi <asbjorn.nisi@hotmail.com>
2022-11-09 13:14:22 +0100

    Fixed DCLF

--- PartA/DCLF_functions.py
+++ PartA/DCLF_functions.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import cmath
 
-from NR_functions import read_buses, printing_jacobian, P_Calc, Q_Calc, get_PQ_calc, delta_VD, updateVD, updateVD_vec, updatePQ_vec, P_Updated, Q_Updated, Q_max_violation, printing_buses, PQ_to_PV
+from NR_functions import Q_violated, printing_jacobian, P_Calc, Q_Calc, get_PQ_calc, delta_VD, updateVD, updateVD_vec, updatePQ_vec, P_Updated, Q_Updated, Q_max_violation, VD_vec_Qmax, Q_calc_violated
 
 
 def Ybus_dclf(file, shape):
@@ -93,7 +93,7 @@ def make_jacobian_dclf(VD_jacobian, PQ_jacobian, PQ_vec, num_buses, V, delta, Yb
     return j
 
 
-
+"""
 def iterate_dclf(VD_jacobian, PQ_jacobian, PQ_vec, PQ_vec_updated, num_buses, V, delta, V_vec, delta_vec, Ybus, bus_num_init, P_init, Q_init, VD_vec_current, power_network, bus_type_init, bus_type):
     #1
     print("VD_vec")
@@ -140,5 +140,65 @@ def iterate_dclf(VD_jacobian, PQ_jacobian, PQ_vec, PQ_vec_updated, num_buses, V,
     PQ_vec_updated = updatePQ_vec(PQ_vec, V_updated, delta_updated, Ybus, bus_num_init, P_init, Q_init)
 
     return PQ_vec_updated, delta_updated, V_updated, VD_vec_current, P_calc, Q_calc, delta_vd
+"""
 
+
+def iterate_dclf(VD_jacobian, PQ_jacobian, PQ_vec, num_buses, V, delta, V_vec, delta_vec, Ybus, bus_num_init, P_init, Q_init, VD_vec_current, power_network, bus_type_init, Q_max):
+     
+    #1 Calculates new values for P and Q separately
+    P_calc = P_Calc(V_vec, Ybus, bus_num_init, delta_vec, P_init)
+    Q_calc = Q_Calc(V_vec, Ybus, bus_num_init, delta_vec, Q_init)
+    
+    #2 Updates RHS of inverted-jacobi-equation 
+    PQ_calc_updated = get_PQ_calc(P_calc, Q_calc) 
+
+    #3 Creating new jacobian
+    j = make_jacobian_dclf(VD_jacobian, PQ_jacobian, PQ_calc_updated, num_buses, V_vec, delta_vec, Ybus)
+    
+    #4 Inverting jacobian 
+    j_inv = np.linalg.inv(j)
+
+    #5 Updates LHS of  inverted-jacobi-equation
+    delta_vd = delta_VD(PQ_vec, PQ_calc_updated, j_inv)
+    
+    #6 Updates values for V and delta
+    VD_vec_current = updateVD_vec(VD_vec_current, delta_vd, bus_type_init, bus_type_init, delta, V)
+    
+    #print('VD_vec_current')
+    #print(VD_vec_current)
+    #VD_vec_current = insert_VD_vec(delta, delta_updated, V, V_updated, VD_vec_current)
+    #print(VD_vec_current)
+
+    #7 Updates V_values and delta_values in separate vectores tougether with given values.  
+    delta_updated, V_updated = updateVD(VD_vec_current,delta, V , bus_type_init, bus_type_init)
+
+
+    #8 New P and Q vectors without nan.
+    P_updated = P_Updated(V_updated, Ybus, bus_num_init, delta_updated)     #Returns P vector updated with calculated values for unknown P's instead of nan.
+    Q_updated = Q_Updated(V_updated, Ybus, bus_num_init, delta_updated)     #Returns Q vector updated with calculated values for unknown Q's instead of nan.
+    
+    #8 Checking Q_max 
+    bus_type = bus_type_init
+    
+    """
+    if(Q_violated(Q_max, Q_updated, bus_type)):
+        Q_updated, power_network = Q_max_violation(Q_updated, Q_max, bus_num_init, V, power_network)
+        bus_type = power_network.get_bus_type_vec()
+        VD_vec, VD_jacobian = power_network.get_VD_jacobian()
+        PQ_vec, PQ_jacobian = power_network.get_PQ_vec()
+        
+        
+        VD_vec_current = VD_vec_Qmax(VD_vec, VD_vec_current, bus_type, bus_type_init, V)
+        
+        
+        V  = power_network.get_V_vec()
+        VD_vec_current = updateVD_vec(VD_vec_current, delta_vd, bus_type_init, bus_type, delta, V)
+
+
+        delta_updated, V_updated = updateVD(VD_vec_current,delta, V, bus_type_init, bus_type)
+        Q_calc = Q_calc_violated(bus_type_init,bus_type, Q_updated, Q_calc)
+    """
+    
+
+    return delta_updated, V_updated, VD_vec_current, P_calc, Q_calc, P_updated, Q_updated, bus_type, power_network, VD_jacobian, PQ_jacobian, PQ_vec, bus_type, delta_vd, V 
 
--- PartA/decoupled_load_flow.py
+++ PartA/decoupled_load_flow.py
@@ -7,98 +7,52 @@ from DCLF_functions import Ybus_dclf, iterate_dclf
 from NR_network import Network
 
 
-bus_vec = read_buses('Part1/Busdata.csv')
+bus_vec = read_buses('PartA/Busdata.csv')
 power_network = Network(bus_vec)
 #print(power_network.get_bus(2))
 
-Ybus = Ybus_dclf('Part1/impedances.csv', 5)
+Ybus = Ybus_dclf('PartA/impedances.csv', 5)
 convergence = 0.00001
 Q_max = [0.5, 5, 1.5,5,5]
 
-
 def DCLF(Ybus, power_network, convergence, Q_max):
-    V_init = power_network.get_V_calc()
-    delta_init = power_network.get_delta_calc()
-    P_init = power_network.get_P_vec()
-    Q_init = power_network.get_Q_vec()
-    bus_num_init = power_network.get_bus_num_vec()
-    V = power_network.get_V_vec()
-    delta = power_network.get_delta_vec()
-    PQ_vec, PQ_jacobian = power_network.get_PQ_vec()
-    VD_vec, VD_jacobian = power_network.get_VD_jacobian()
-    bus_type_init = power_network.get_bus_type_vec()
-    num_buses = len(bus_num_init)
+    V_init = power_network.get_V_calc()   #Appends 1 if nan. Otherwise given value 
+    delta_init = power_network.get_delta_calc()  #Appends 0 if nan. Otherwise given value 
+    P_init = power_network.get_P_vec() #Returns P_values. If unknown, returns nan
+    Q_init = power_network.get_Q_vec() #Returns Q_values. If unknown, returns nan
+    bus_num_init = power_network.get_bus_num_vec()  #Returns bus number as a vector (Not intitial)
+    V = power_network.get_V_vec() #Returns V_values. If unknown, returns nan
+    delta = power_network.get_delta_vec() #Returns delta_values. If unknown, returns nan
+    PQ_vec, PQ_jacobian = power_network.get_PQ_vec() #Returns PQ_vec. This is a vector consisting of the kwnown values of P and Q. P_values first, then Q_values. (LHS of jacobi-equation) 
+                                                     #Returns PQ_jacobian. Tis is av vactor of "PQ"-objects which contains information if element is P or Q, and what kind of bus the P and Q bellongs to. 
+    VD_vec, VD_jacobian = power_network.get_VD_jacobian() #Returns VD_vec. This vector consists of unknown elements of V and delta. This elemens are given flat start values. 1 if V, 0 if delta. This is only valid before the first iteration. Delta first, then V. 
+                                                          #Returns VD:jacobian. This vector consists of "VD"-objects which contains information if element is V or delta, and what kind of bus the V and delta bellongs to.
+    bus_type_init = power_network.get_bus_type_vec() #Returns vector with bus type. O if slack, 1 if PV and 2 if PQ.       
+    num_buses = len(bus_num_init)  #Calculates how many busses there are in the network. 
     
-
-    delta_vd = [1,1,1,1,1,1,1]
-    PQ_vec_updated = PQ_vec.copy()
+    delta_vd = [1] * len(VD_vec) #Initial state of delta_vd. To avoid convergens in first iteration. 
     i= 0
-    #print("V")
-    #print(V)
     while(abs(max(np.real(delta_vd))) > convergence):
-        if (i==0):
+        if (i==0):  #First iteration
             print("Iteration", i+1, ": \n")
             bus_type = power_network.get_bus_type_vec()
-            PQ_vec_updated, delta_updated, V_updated, VD_vec_current, P_calc, Q_calc, delta_vd = iterate_dclf(VD_jacobian, PQ_jacobian, PQ_vec, PQ_vec, num_buses, V, delta, V_init, delta_init, Ybus, bus_num_init, P_init, Q_init, VD_vec, power_network, bus_type_init, bus_type)
+            delta_updated, V_updated, VD_vec_current, P_calc, Q_calc, P_updated, Q_updated, bus_type, power_network, VD_jacobian, PQ_jacobian, PQ_vec, bus_type, delta_vd, V  = iterate_dclf(VD_jacobian, PQ_jacobian, PQ_vec, num_buses, V, delta, V_init, delta_init, Ybus, bus_num_init, P_init, Q_init, VD_vec, power_network, bus_type_init, Q_max)
             i += 1
-            #print("V_updated")
-            #print(V_updated)
-            #print("delta_updated")
-            #print(delta_updated)
-            P_updated = P_Updated(V_updated, Ybus, bus_num_init, delta_updated)
-            Q_updated = Q_Updated(V_updated, Ybus, bus_num_init, delta_updated)
-            printing_buses(V_updated, delta_updated, P_updated, Q_updated, bus_num_init, bus_type_init)
-            """
-            Q_updated, power_network = Q_max_violation(Q_updated, Q_max, bus_num_init, V, power_network)
-            power_network = PQ_to_PV(bus_type_init, Q_updated, Q_max, V_updated, power_network)
-            V = power_network.get_V_vec()
-            Q_calc = power_network.get_Q_vec()
-            VD_vec, VD_jacobian = power_network.get_VD_jacobian()
-            PQ_vec, PQ_jacobian = power_network.get_PQ_vec()
-            delta = power_network.get_delta_calc()
-            PQ_vec, PQ_jacobian = power_network.get_PQ_vec()
-            VD_vec, VD_jacobian = power_network.get_VD_jacobian()
-            bus_type_init = power_network.get_bus_type_vec()
             
-            print("V")
-            #print(V)
-            print(VD_vec)
-            """
-
-            #print(V_updated)
-            #print(Q_calc)
-        
-        elif (i==5):
+            printing_buses(V_updated, delta_updated, P_updated, Q_updated, bus_num_init, bus_type)
+        elif (i==10):
             break
         else:
             print("Iteration", i+1, ": \n")
-            PQ_vec_updated, delta_updated, V_updated, VD_vec_current, P_calc, Q_calc, delta_vd = iterate_dclf(VD_jacobian, PQ_jacobian, PQ_vec, PQ_vec_updated, num_buses, V, delta, V_updated, delta_updated, Ybus, bus_num_init, P_calc, Q_calc, VD_vec_current,power_network, bus_type_init, bus_type)
-            P_updated = P_Updated(V_updated, Ybus, bus_num_init, delta_updated)
-            Q_updated = Q_Updated(V_updated, Ybus, bus_num_init, delta_updated)
-            printing_buses(V_updated, delta_updated, P_updated, Q_updated, bus_num_init, bus_type_init)
-            """
-            Q_updated, power_network = Q_max_violation(Q_updated, Q_max, bus_num_init, V, power_network)
-            power_network = PQ_to_PV(bus_type_init, Q_updated, Q_max, V_updated, power_network)
-            print("V_updated")
-            print(V_updated)
-            V = power_network.get_V_vec()
-            Q_calc = power_network.get_Q_vec()
-            VD_vec, VD_jacobian = power_network.get_VD_jacobian()
-            PQ_vec, PQ_jacobian = power_network.get_PQ_vec()
-            delta = power_network.get_delta_calc()
-            PQ_vec, PQ_jacobian = power_network.get_PQ_vec()
-            VD_vec, VD_jacobian = power_network.get_VD_jacobian()
-            bus_type_init = power_network.get_bus_type_vec()
-            """
+            delta_updated, V_updated, VD_vec_current, P_calc, Q_calc, P_updated, Q_updated, bus_type, power_network, VD_jacobian, PQ_jacobian, PQ_vec, bus_type, delta_vd, V  = iterate_dclf(VD_jacobian, PQ_jacobian, PQ_vec, num_buses, V, delta, V_updated, delta_updated, Ybus, bus_num_init, P_calc, Q_calc, VD_vec_current,power_network, bus_type, Q_max)            
+            printing_buses(V_updated, delta_updated, P_updated, Q_updated, bus_num_init, bus_type)
             i += 1
-    return PQ_vec_updated, P_updated, Q_updated 
+    return P_updated, Q_updated 
       
 
 
-a, b, c = DCLF(Ybus, power_network, convergence, Q_max)
+a, b = DCLF(Ybus, power_network, convergence, Q_max)
 
 print(a)
 print(b)
-print(c)
-
 
