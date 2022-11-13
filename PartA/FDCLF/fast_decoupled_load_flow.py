424f72930d7bb313f1fe9bfb35a36597b6129982
Durolicana <115071928+Durolicana@users.noreply.github.com>
2022-11-13 17:53:41 +0100

    Finpuss Q_max

--- PartA/FDCLF/fast_decoupled_load_flow.py
+++ PartA/FDCLF/fast_decoupled_load_flow.py
@@ -60,13 +60,6 @@ def FDCLF(Ybus, power_network, convergence, Q_max, method, Q_limit, reactive_lim
             print("Iteration", i+1, ": \n")
             V_updated, delta_updated, delta_Delta, delta_V, P_updated, Q_updated, V_vec_1_updated, V_vec_2_updated, power_network, bus_type_vec, Q_vec_FD, P_vec_FD = iterate_fdclf(num_buses, bus_num_init, V_updated, V_vec_1_updated, V_vec_2_updated, delta_updated, delta_updated, Ybus, bus_type_vec, P_vec_FD, Q_vec_FD, Q_max, power_network, method, Q_limit, reactive_limits_method)
             printing_buses(V_updated, delta_updated, P_updated, Q_updated, bus_num_init, bus_type_vec)
-            print("Delta")
-            print(delta_Delta)
-            print(delta_V)
-            print("Konvergens")
-            print(convergence)
-            print(abs(np.real(max(delta_V))))
-            print(abs(np.real(max(delta_V))) > convergence)
     
     printing_lines(bus_vec, "PartA/impedances.csv", V_updated, Ybus, delta_updated)
     
